6cf8c49d3c9dcfc5712c280f70a7b7207cf2522f
Mohammad Zakaria <zakariayus10@gmail.com>
2022-10-26 18:31:57 +0700

    Testing API Commit

--- src/test/java/com/juaracoding/duadua/GetSingle.java
+++ src/test/java/com/juaracoding/duadua/GetSingle.java
@@ -11,7 +11,7 @@ import static org.hamcrest.Matchers.equalTo;
 public class GetSingle {
     String endpoint = "https://mern-backend-8881.herokuapp.com/products/63591637f3b6006945f67354";
 
-    @Test
+    @Test(priority = 1)
     public void testStatusCode() {
         Response response = RestAssured.get(endpoint);
         System.out.println(response.getBody().asString());
@@ -22,6 +22,17 @@ public class GetSingle {
 
         int actual = response.getStatusCode();
         Assert.assertEquals(actual, 200);
+    }
+
+    @Test(priority = 2)
+    public void testGetlist() {
+        given()
+                .get(endpoint)
+                .then()
+                .statusCode(200)
+                .body("name", equalTo("GTX 1660 Super"))
+                .body("category", equalTo("Graphic card "))
+                .body("price", equalTo(229));
     }
 
 }
